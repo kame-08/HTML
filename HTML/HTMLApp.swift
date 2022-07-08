a69cebfa8d04c8b91a4dc8c2848810f3df1e6fbc
Ryo <21cm0108@jec.ac.jp>
2022-07-08 22:57:55 +0900

    Initial Commit

---
+++ HTML/HTMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  HTMLApp.swift
+//  HTML
+//
+//  Created by Ryo on 2022/07/08.
+//
+
+import SwiftUI
+
+@main
+struct HTMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
